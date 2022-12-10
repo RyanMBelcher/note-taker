b3c1c89c497e54d2f59c4b630369efc42972f110
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-10 16:04:32 -0500

    added helper function

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+const uuid = require('./helper/helper');
 
 const PORT = 3001;
 // const jsonData = require('./Develop/db/db.json');
 
 const app = express();
 
-app.use(express.json('public'));
+app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 
 
 // HTML routes
@@ -17,15 +22,15 @@ app.get('/notes', (req, res) =>
 );
 
 // returns index.html
-app.get('*', (req, res) =>
+app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 // API routes
 // reads db.json file and returns all saved notes as json
-// app.get('/api/notes', (req, res) =>
-//     res.json(jsonData)
-// );
+app.get('/api/notes', (req, res) =>
+    res.json(jsonData)
+);
 
 // receives a new note to save on the request body, add to db.json file, and then return
 // note to client. Each note will need a unique id.
