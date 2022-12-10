557d8741d7b05a5cd08ed46ee6e4b0a01ffa7d7e
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-10 18:24:20 -0500

    worked on app.post

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
 
 const uuid = require('./helper/helper');
 
@@ -28,17 +29,48 @@ app.get('/', (req, res) =>
 
 // API routes
 // reads db.json file and returns all saved notes as json
-app.get('/api/notes', (req, res) =>
-    res.json(jsonData)
-);
+app.get('/api/notes', (req, res) => {
+    res.json(`${req.method} request received to get notes`);
+
+    console.info(`${req.method} request received to get notes`);
+});
 
 // receives a new note to save on the request body, add to db.json file, and then return
 // note to client. Each note will need a unique id.
-// app.post('/api/notes'(req, res) =>
-//     res.json()
-// );
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note.`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid()
+        };
+        const noteString = JSON.stringify(newNote);
+
+        fs.writeFile(`./db/${newNote.note}.json`, noteString, (err) =>
+            err
+                ? console.error(err)
+                : console.log(`Note for ${newNote.note} has been written to JSON file`)
+        );
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+
+}
+);
 
 
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 });
