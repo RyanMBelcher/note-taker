514a41272590c6c7eea2a2d861c22ba3d23f584a
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-12 20:17:20 -0500

    added app.delete

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const util = require('util');
 const uuid = require('uuid');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // middleware methods
@@ -26,11 +26,9 @@ const readFromFile = util.promisify(fs.readFile);
 
 // API routes
 // reads db.json file and returns all saved notes as json
-app.get('/api/notes', (req, res) => {
-    // res.json(`${req.method} request received to get notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-
-});
+app.get('/api/notes', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
 
 // receives a new note to save on the request body, add to db.json file, and then return
 // note to client. Each note will need a unique id.
@@ -75,8 +73,34 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(JSON.parse(data));
+        }
+
+        const notes = JSON.parse(data);
+
+        const filteredNotes = notes.filter(note => {
+            return id !== note.id
+        })
+        fs.writeFile('./db/db.json', JSON.stringify(filteredNotes, null, '\t'), (err) =>
+            err
+                ? console.err(err)
+                : console.log(`Note has been written to JSON file`)
+        )
+        res.sendFile(path.join(__dirname, 'public/index.html'))
+    })
+});
 
 
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT} 🚀`)
